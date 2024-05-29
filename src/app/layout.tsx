0634106792c8fd10f8fe2064488d96988070ae93
Chigozie Oduah <87097037+GhoulKingR@users.noreply.github.com>
2024-05-29 20:35:17 +0100

    enable user zooming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import type {Metadata, Viewport} from 'next';
-import {Inter} from 'next/font/google';
+import type { Metadata, Viewport } from 'next';
+import { Inter } from 'next/font/google';
 import './globals.css';
 
-const inter = Inter({subsets: ['latin']});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -13,7 +13,6 @@ export const viewport: Viewport = {
     width: 'device-width',
     initialScale: 1,
     maximumScale: 1,
-    userScalable: false,
 };
 
 export default function RootLayout({
